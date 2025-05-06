7a9724071dab9b58f6ea10ce5d343c62160af349
Ritish2424 <dankritish69@gmail.com>
2025-05-06 18:03:28 +0000

    Datatypes

---
+++ 1_Basics/Datatypes.js
@@ -0,0 +1,14 @@
+"use strict";
+// alert(3+3)
+
+console.log(3+3);
+console.log("Ritish")
+
+let name="Ritish"
+let age=18
+// let isLoggedIn=False
+let State;
+console.log(typeof "Ritish")
+console.log(typeof age)
+console.log(typeof undefined)
+console.log(typeof null)
---
+++ 1_Basics/part1.js
@@ -0,0 +1,14 @@
+const accountId=144553
+let accocutnEmail="ritish@google.com"
+var accountPassword="2424"
+accountCity="Delhi"
+let accountState;
+
+// accountId=2
+
+
+accountEmail="ttt@google.in"
+accountPassword="987897"
+accountCity="Kerla"
+console.log(accountId);
+console.table([accountId, accountEmail , accountPassword , accountCity, accountState])
