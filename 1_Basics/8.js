87950a8d7686c2aa43a151fe08545f28ab121d52
Ritish2424 <dankritish69@gmail.com>
2025-05-08 18:26:44 +0000

    Datatype summary

---
+++ 1_Basics/8.js
@@ -0,0 +1,40 @@
+const score=100
+const scoreValue=100.3
+const isLoggedIn=false
+const OutsideTemp= null
+
+
+console.log(typeof OutsideTemp)
+// let userEmail;
+
+let UserEmail=undefined
+let userEmail;
+console.log(typeof UserEmail)
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id===anotherId);
+
+
+
+const BigNumber=78974784848489477n
+console.log(typeof BigNumber)
+
+
+const heros=["Ritish","RitishK" ,"RitishKanojia"]
+console.log(typeof heros)
+
+
+
+ let myObj={
+    name: "Ritish" ,
+    age : 21 }
+
+    console.log(typeof myObj)
+
+
+
+const myFunction=function(){
+    console.log("Hello World")
+}
+console.log(typeof OutsideTemp)
