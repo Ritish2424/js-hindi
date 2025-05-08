9c5bb50afa3e5b9e389f3ff4a304d1837f492559
Ritish2424 <dankritish69@gmail.com>
2025-05-08 17:22:48 +0000

    Datatypes converisons

---
+++ 1_Basics/6.js
@@ -0,0 +1,18 @@
+// let score="33abc"
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// let isLoggedIn="Ritish"
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber=33
+let stringNumber= String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
