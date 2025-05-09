efc4f0ee84ecfee15420f707bac57d78d34165a2
Ritish2424 <dankritish69@gmail.com>
2025-05-09 17:00:44 +0000

    Stack And Heap

---
+++ 1_Basics/9.js
@@ -0,0 +1,17 @@
+let MyYoutubename="GameLikeRitish"
+let anothername=MyYoutubename
+anothername="ChaiAurCode"
+
+console.log(MyYoutubename)
+console.log(anothername)
+
+
+let userOne={
+    email :"user@google.com",
+    upi: "usery@ybl.uk"
+}
+let userTwo = userOne
+userTwo.email="Ritishkanojia@.uk"
+
+console.log(userOne.email);
+console.log(userTwo.email);
